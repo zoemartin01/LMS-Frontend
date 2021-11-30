95e9363c7d9daa0f09e7716c42997ee42537cf58
Adrian Keller <adrian@adrian-keller.de>
2021-11-30 01:48:19 +0100

    added app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  unreadMessages: number = 0;
+  loggedIn: boolean = false;
+  isAdmin: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.unreadMessages = 7;
+    this.loggedIn = true;
+    this.isAdmin = true;
+  }
 }
