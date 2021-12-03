6a495566de61afe3807978c69963857ecfc062e5
Adrian Keller <adrian@adrian-keller.de>
2021-12-03 16:12:50 +0100

    added JSDoc to messaging service

--- src/app/services/messaging.service.ts
+++ src/app/services/messaging.service.ts
@@ -13,18 +13,40 @@ export class MessagingService {
   constructor(private httpClient: HttpClient) {
   }
 
+  /**
+   * Retrieves all messages for current user
+   */
   public getMessages(): Observable<any> {
   }
 
+  /**
+   * Retrieves the amounts of unread messages for current user
+   */
   public getUnreadMessagesAmounts(): Observable<any> {
   }
 
+  /**
+   * Sends a message to another user
+   *
+   * @param recipient recipient of the message
+   * @param message   contents of the message
+   */
   public sendMessage(recipient: UserId, message: Message): Observable<any> {
   }
 
+  /**
+   * Deletes a message from database
+   *
+   * @param messageId id of concerned message
+   */
   public deleteMessage(messageId: number): Observable<any> {
   }
 
+  /**
+   * Marks a message as read
+   *
+   * @param messageId id of concerned message
+   */
   public markMessageAsRead(messageId: number): Observable<any> {
   }
 }
