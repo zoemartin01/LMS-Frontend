3691b3f57bda611bf1a365b24eda7100ba5891dc
Adrian Keller <adrian@adrian-keller.de>
2021-12-03 16:27:09 +0100

    added JSDoc

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,13 +13,27 @@ export class AuthService {
   constructor(private httpClient: HttpClient) {
   }
 
+  /**
+   * Retrieves user details
+   */
   public getUserDetails(): Observable<any> {
   }
 
+  /**
+   * Returns full name of specified user
+   *
+   * @param user a user
+   */
   public getFullName(user: User): string {
     return `${user.firstname} ${user.lastname}`;
   }
 
+  /**
+   * Logs in user with specified credentials
+   *
+   * @param email    user's email address
+   * @param password user's password
+   */
   public login(email: string, password: string): Observable<any> {
     const apiURL = `${environment.baseUrl}${environment.apiRoutes.loginRoute}`;
     const requestBody = {
@@ -30,6 +44,9 @@ export class AuthService {
     return this.httpClient.post(apiURL, requestBody);
   }
 
+  /**
+   * Logs out current user
+   */
   public logout(): Observable<any> {
     const apiURL = `${environment.baseUrl}${environment.apiRoutes.logoutRoute}`;
     const httpHeaders: HttpHeaders = new HttpHeaders({
@@ -39,6 +56,9 @@ export class AuthService {
     return this.httpClient.delete(apiURL, {headers: httpHeaders});
   }
 
+  /**
+   * Refreshs authentication token of current user
+   */
   public tokenRefresh(): Observable<any> {
     const apiURL = `${environment.baseUrl}${environment.apiRoutes.tokenRefreshRoute}`;
     const requestBody = {
@@ -48,6 +68,9 @@ export class AuthService {
     return this.httpClient.post(apiURL, {headers: requestBody});
   }
 
+  /**
+   * Checks token of current user
+   */
   public tokenCheck(): Observable<any> {
     const apiURL = `${environment.baseUrl}${environment.apiRoutes.tokenTestRoute}`;
     const httpHeaders: HttpHeaders = new HttpHeaders({
@@ -57,40 +80,84 @@ export class AuthService {
     return this.httpClient.post(apiURL, {headers: httpHeaders});
   }
 
+  /**
+   * Signs in user with his personal information
+   *
+   * @param firstname new user's firstname
+   * @param lastname  new user's lastname
+   * @param email     new user's email address
+   * @param password  new user's password
+   */
   public signin(firstname: string, lastname: string, email: string, password: string): Observable<any> {
   }
 
+  /**
+   * Verifies email address using a token sent on signin
+   *
+   * @param userId user's id
+   * @param token  token to verify email
+   */
   public verifyEmail(userId: UserId, token: string): Observable<any> {
   }
 
+  /**
+   * Saves access token in local storage
+   *
+   * @param accessToken new value of access token
+   */
   public setAccessToken(accessToken: string): void {
     localStorage.setItem(environment.storageKeys.accessToken, accessToken);
   }
 
+  /**
+   * Returns access token from local storage
+   */
   public getAccessToken(): string {
     return <string>localStorage.getItem(environment.storageKeys.accessToken);
   }
 
+  /**
+   * Saves refresh token in local storage
+   *
+   * @param refreshToken new value of refresh token
+   */
   public setRefreshToken(refreshToken: string): void {
     localStorage.setItem(environment.storageKeys.refreshToken, refreshToken);
   }
 
+  /**
+   * Returns refresh token from local storage
+   */
   public getRefreshToken(): string {
     return <string>localStorage.getItem(environment.storageKeys.refreshToken);
   }
 
+  /**
+   * Saves user role in local storage
+   *
+   * @param userRole
+   */
   public setUserRole(userRole: string): void {
     localStorage.setItem(environment.storageKeys.userRole, userRole);
   }
 
+  /**
+   * Returns user role from local storage
+   */
   public getUserRole(): string {
     return <string>localStorage.getItem(environment.storageKeys.userRole);
   }
 
+  /**
+   * Returns if current user is logged in
+   */
   public isUserLoggedIn(): boolean {
     return this.getAccessToken() !== null;
   }
 
+  /**
+   * Returns if current user is admin
+   */
   public isAdmin(): boolean {
     return this.getUserRole() === "admin";
   }
