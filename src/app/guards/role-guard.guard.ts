d0204f1f9a5bd5a01c7caab583c3a28727da2963
Adrian Keller <adrian@adrian-keller.de>
2021-12-03 16:30:36 +0100

    worked on role guard

--- src/app/guards/role-guard.guard.ts
+++ src/app/guards/role-guard.guard.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Router, CanActivate, ActivatedRouteSnapshot, UrlTree} from '@angular/router';
-import {AuthService} from '../services/auth.service';
 import {Observable} from 'rxjs';
+
+import {AuthService} from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +12,8 @@ export class RoleGuard implements CanActivate {
   constructor(public authService: AuthService, public router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    const expectedRoleArray = route.data['expectedRole'];
-
-    const userRole = this.authService.getUserRole();
-
-    let expectedRole;
-    for (expectedRole of expectedRoleArray) {
-      if (expectedRole === userRole){
-        return true;
-      }
+    if (this.authService.isAdmin()){
+      return true;
     }
 
     return this.router.parseUrl('/dashboard');
