e1d9f7af6267201072894342010652c790258de7
Adrian Keller <adrian@adrian-keller.de>
2021-12-03 02:08:11 +0100

    created message service and corresponding types

--- src/app/components/message-box/message-box.component.ts
+++ src/app/components/message-box/message-box.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Message } from "../../types/message";
+import {UnreadMessages} from "../../types/unread-messages";
+
 @Component({
   selector: 'app-message-box',
   templateUrl: './message-box.component.html',
   styleUrls: ['./message-box.component.scss']
 })
 export class MessageBoxComponent implements OnInit {
+  public messages: Message[] = [];
+  public unreadMessages: UnreadMessages = {
+    sum: 0,
+    appointments: 0,
+    orders: 0,
+    users: 0,
+  };
 
   constructor() {
   }
---
+++ src/app/services/messaging.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+import { Message } from "../types/message";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagingService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public getMessages(): Observable<any> {
+  }
+
+  public getUnreadMessagesAmounts(): Observable<any> {
+  }
+
+  public addMessage(message: Message): Observable<any> {
+  }
+
+  public deleteMessage(messageId: number): Observable<any> {
+  }
+
+  public markMessageAsRead(messageId: number): Observable<any> {
+  }
+}
