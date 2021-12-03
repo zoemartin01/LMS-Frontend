b5f4c547de0a0e3cc9bd043aed2f55481614b24c
Adrian Keller <adrian@adrian-keller.de>
2021-12-03 01:05:03 +0100

    added rout for homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,18 @@ import { RoleGuard } from "./guards/role-guard.guard";
 import { AuthGuard } from "./guards/auth.guard";
 
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HomepageComponent } from "./components/homepage/homepage.component";
 import { LoginComponent } from "./components/login/login.component";
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+    pathMatch: 'full',
+    data: {
+      title: 'Homepage'
+    }
+  },
   {
     path: 'login',
     component: LoginComponent,
