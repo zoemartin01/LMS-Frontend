9bfacfea43ba206c0fd7d80780d4e0327338e274
Lena <lena.richau@gmail.com>
2021-12-05 23:05:39 +0100

    added components for order management without functionality

---
+++ src/app/components/order-management/request-creation/request-creation.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RequestCreationComponent } from './request-creation.component';
+
+describe('RequestCreationComponent', () => {
+  let component: RequestCreationComponent;
+  let fixture: ComponentFixture<RequestCreationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RequestCreationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RequestCreationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/order-management/request-creation/request-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-creation',
+  templateUrl: './request-creation.component.html',
+  styleUrls: ['./request-creation.component.scss']
+})
+export class RequestCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
