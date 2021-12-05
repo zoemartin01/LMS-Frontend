749f8e390d66416b9258a5ce82fe13fa46cb7632
Zoe Martin <git@zoemartin.me>
2021-12-05 20:51:41 +0100

    add livecam overview methods

--- src/app/livecam/overview/overview.component.ts
+++ src/app/livecam/overview/overview.component.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
+  public recordings: any[] = [];
 
   constructor() { }
 
+  /**
+   * Init page
+   */
   ngOnInit(): void {
   }
 
+  /**
+   * Gets recording data
+   */
+  public async getRecordings(): Promise<void> {
+  }
+
+  /**
+   * Gets live stream feed
+   */
+  public async getLiveStreamFeed(): Promise<void> {
+  }
+
+  /**
+   * Download a recording
+   * 
+   * @param recordingId id of recording
+   */
+  public async downloadRecording(recordingId: any): Promise<void> {
+  }
+
+  /**
+   * Open recording deletion dialog
+   * 
+   * @param recordingId id of recording
+   */
+  public deleteRecording(recordingId: any): void {
+  }
+
+  /**
+   * Open recording schedule dialog
+   */
+  public scheduleRecording(): void {
+  }
 }
