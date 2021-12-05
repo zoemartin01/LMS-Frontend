06147228074e3c78152054f93c77d35d177e256f
Adrian Keller <adrian@adrian-keller.de>
2021-12-05 11:10:28 +0100

    fixed types and codestyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from "./services/auth.service";
-import {UnreadMessages} from "./types/unread-messages";
+import { UnreadMessages } from "./types/unread-messages";
 
 @Component({
   selector: 'app-root',
--- src/app/types/message.ts
+++ src/app/types/message.ts
@@ -1,8 +1,11 @@
+import { MessageId } from "./aliases/message-id";
+
 export interface Message {
+  id: MessageId,
   title: string,
   content: string,
   link: {
     url: string,
-    text: string
+    text: string,
   },
 }
