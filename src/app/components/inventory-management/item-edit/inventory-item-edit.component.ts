8cdd942c43cfc1d15839069daff87235122bd614
Lena <lena.richau@gmail.com>
2021-12-07 00:31:47 +0100

    added method stubs for inventory item create component

--- src/app/components/inventory-management/item-edit/inventory-item-edit.component.ts
+++ src/app/components/inventory-management/item-edit/inventory-item-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+
+import { InventoryService } from "../../../services/inventory.service";
+import { Item } from "../../../types/item";
+
 
 @Component({
   selector: 'app-item-edit',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryItemEditComponent implements OnInit {
 
-  constructor() {
+  // do we really need route here? don't really get what for
+  constructor(public inventoryService: InventoryService,  private route: ActivatedRoute) {
   }
 
+  /**
+   * Init page
+   */
   ngOnInit(): void {
+    // not sure if here should be the code for route stuff
+    // will leave the method stub like this...?
+  }
+
+  /**
+   * Get all data of item
+   */
+  public async getItemData() : Promise<void> {
+  }
+
+  /**
+   * Changes data of item
+   *
+   * @param itemEditForm submitted create form
+   */
+  public async editItemData(itemEditForm: NgForm): Promise<void> {
   }
 }
