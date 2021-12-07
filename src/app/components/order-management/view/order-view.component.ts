d2551956f55581a5a768d9acd5d1b22dd1f1d5f0
Lena <lena.richau@gmail.com>
2021-12-07 21:25:50 +0100

    added method stubs to order view component

--- src/app/components/order-management/view/order-view.component.ts
+++ src/app/components/order-management/view/order-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
+
+import {InventoryService} from "../../../services/inventory.service";
 
 @Component({
   selector: 'app-order-view',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderViewComponent implements OnInit {
 
-  constructor() {
+  constructor(public inventoryService: InventoryService, private route: ActivatedRoute) {
   }
 
+  /**
+   * Init page
+   */
   ngOnInit(): void {
   }
+
+  /**
+   * Get all data of order
+   */
+  private async getOrderData() : Promise<void> {
+  }
+
+  /**
+   * Opens order edit form
+   *
+   * @param {NgForm} orderEditForm submitted edit form
+   */
+  public openOrderEditForm(orderEditForm: NgForm): void {
+  }
+
+  /**
+   * Opens order delete confirmation popup
+   */
+  public openOrderDeletionDialog(): void {
+  }
+
 }
