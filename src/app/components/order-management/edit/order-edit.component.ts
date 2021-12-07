8206fe63dafa01618e6da4fb52e75fcdde55ce35
Lena <lena.richau@gmail.com>
2021-12-07 21:10:29 +0100

    added method stubs to order edit component

--- src/app/components/order-management/edit/order-edit.component.ts
+++ src/app/components/order-management/edit/order-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
+
+import {OrderService} from "../../../services/order.service";
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderEditComponent implements OnInit {
 
-  constructor() {
+  constructor(public orderService: OrderService,  private route: ActivatedRoute) {
   }
 
+  /**
+   * Init page
+   */
   ngOnInit(): void {
   }
+
+  /**
+   * Get all data of order
+   */
+  private async getOrderData() : Promise<void> {
+  }
+
+  /**
+   * Opens form to change data of order
+   *
+   * @param {NgForm} orderEditForm submitted edit form
+   */
+  public async openOrderEditForm(orderEditForm: NgForm): Promise<void> {
+  }
+
 }
