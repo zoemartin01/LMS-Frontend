c6de0e44d2f6d9f5a8d421bad9a7c23e71c32855
Lena <lena.richau@gmail.com>
2021-12-07 21:15:48 +0100

    added method stubs to inventory order component

--- src/app/components/order-management/inventory-order/inventory-order.component.ts
+++ src/app/components/order-management/inventory-order/inventory-order.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
 
+import {InventoryService} from "../../../services/inventory.service";
+
 
 @Component({
   selector: 'app-inventory-order',
@@ -9,9 +11,26 @@ import { NgForm } from "@angular/forms";
 })
 export class InventoryOrderComponent implements OnInit {
 
-  constructor() {
+  constructor(inventoryService: InventoryService) {
   }
 
+  /**
+   * Init page
+   */
   ngOnInit(): void {
+  }
+
+  /**
+   * Get all data of order
+   */
+  private async getOrderData() : Promise<void> {
+  }
+
+  /**
+   * Opens form to put data of order in a new inventory item
+   *
+   * @param {NgForm} itemCreationForm submitted edit form
+   */
+  public async openItemCreationForm(itemCreationForm: NgForm): Promise<void> {
   }
 }
