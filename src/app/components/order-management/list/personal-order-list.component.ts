9d8d0afb3a48db4d4b4b9bdabe33e4f29830ed99
Lena <lena.richau@gmail.com>
2021-12-07 21:18:23 +0100

    added method stubs to personal order list component

--- src/app/components/order-management/list/personal-order-list.component.ts
+++ src/app/components/order-management/list/personal-order-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {OrderId} from "../../../types/aliases/order-id";
+import {OrderService} from "../../../services/order.service";
+import {Order} from "../../../types/order";
 
 @Component({
   selector: 'app-personal-order-list',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-order-list.component.scss']
 })
 export class PersonalOrderListComponent implements OnInit {
+  public orderList: Order[] = [];
 
-  constructor() {
+  constructor(public orderService: OrderService) {
   }
 
+  /**
+   * Init page
+   */
   ngOnInit(): void {
+  }
+
+  /**
+   * Lists all orders with data
+   */
+  private async getInventory(): Promise<void> {
+  }
+
+  /**
+   * Opens form to create order
+   *
+   * @param {NgForm} orderCreationForm submitted create form
+   */
+  public openOrderCreationForm(orderCreationForm: NgForm): void {
+  }
+
+  /**
+   * Opens order edit form
+   *
+   * @param orderId id of order to edit
+   * @param {NgForm} orderEditForm submitted edit form
+   */
+  public openOrderEditForm(orderId: OrderId, orderEditForm: NgForm): void {
+  }
+
+  /**
+   * Opens order delete confirmation popup
+   *
+   */
+  public openOrderDeletionDialog(): void {
+  }
+
+  /**
+   * Opens order view popup
+   *
+   * @param orderId
+   */
+  public openOrderView(orderId: OrderId): void {
   }
 }
