98994ff62d45625e048b96d10c56736645aa2534
Lena <lena.richau@gmail.com>
2021-12-07 21:07:59 +0100

    added method stubs to admin order list component

--- src/app/components/order-management/admin-list/admin-order-list.component.ts
+++ src/app/components/order-management/admin-list/admin-order-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+
+import {Order} from "../../../types/order";
+import {OrderService} from "../../../services/order.service";
+import {OrderId} from "../../../types/aliases/order-id";
 
 @Component({
   selector: 'app-admin-order-list',
@@ -6,10 +12,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-order-list.component.scss']
 })
 export class AdminOrderListComponent implements OnInit {
+  public orderList: Order[] = [];
 
-  constructor() {
+  constructor(public orderService: OrderService) {
   }
 
+  /**
+   * Init page
+   */
   ngOnInit(): void {
   }
+
+  /**
+   * Lists all orders with data
+   */
+  private async getInventory(): Promise<void> {
+  }
+
+  /**
+   * Opens form to create order
+   *
+   * @param {NgForm} orderCreationForm submitted create form
+   */
+  public openOrderCreationForm(orderCreationForm: NgForm): void {
+  }
+
+  /**
+   * Opens order edit form
+   *
+   * @param orderId id of order to edit
+   * @param {NgForm} orderEditForm submitted edit form
+   */
+  public openOrderEditForm(orderId: OrderId, orderEditForm: NgForm): void {
+  }
+
+  /**
+   * Opens order delete confirmation popup
+   *
+   */
+  public openOrderDeletionDialog(): void {
+  }
+
+  /**
+   * Opens order view popup
+   *
+   * @param orderId
+   */
+  public openOrderView(orderId: OrderId): void {
+  }
+
 }
