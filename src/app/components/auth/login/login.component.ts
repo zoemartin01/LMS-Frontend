74130e881edab03a79cc46ffbed86ae5a9a9760b
Adrian Keller <adrian@adrian-keller.de>
2021-12-15 15:06:17 +0100

    refactored auth components

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 
-import { AuthService } from "../../services/auth.service";
+import { AuthService } from "../../../services/auth.service";
 
 @Component({
   selector: 'app-login',
