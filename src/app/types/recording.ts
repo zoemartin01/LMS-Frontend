5371f202f32028f2b16720355a8636c072012a19
Zoe Martin <git@zoemartin.me>
2021-12-20 13:01:03 +0100

    update recording type

--- src/app/types/recording.ts
+++ src/app/types/recording.ts
@@ -1,5 +1,11 @@
 import { RecordingId } from "./aliases/recording-id";
+import { VideoResolution } from "./enums/video-resolution";
 
 export interface Recording {
     id: RecordingId,
+    start: moment.Moment|null,
+    end: moment.Moment|null,
+    resolution: VideoResolution,
+    bitrate: number,
+    size: number,
 }
