b48a491e91b51e94db339cfd9c675a783d2b3862
Zoe Martin <git@zoemartin.me>
2021-12-20 13:02:32 +0100

    forgot userid

--- src/app/types/recording.ts
+++ src/app/types/recording.ts
@@ -1,8 +1,10 @@
 import { RecordingId } from "./aliases/recording-id";
+import { UserId } from "./aliases/user-id";
 import { VideoResolution } from "./enums/video-resolution";
 
 export interface Recording {
     id: RecordingId,
+    userId: UserId,
     start: moment.Moment|null,
     end: moment.Moment|null,
     resolution: VideoResolution,
