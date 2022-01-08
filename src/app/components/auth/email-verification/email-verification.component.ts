d3b5deac1be4718dee667df5dbffb7146f633f42
Adrian Keller <adrian@adrian-keller.de>
2022-01-08 23:53:58 +0100

    fixed some stuff for email verification

--- src/app/components/auth/email-verification/email-verification.component.ts
+++ src/app/components/auth/email-verification/email-verification.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
 
 import { UserService } from "../../../services/user.service";
 
@@ -14,14 +15,31 @@ import { UserService } from "../../../services/user.service";
  *
  *
  */
-export class EmailVerificationComponent {
+export class EmailVerificationComponent implements OnInit {
+  public showForm: boolean = false;
+
 
   /**
    * Constructor
    * @constructor
    * @param {UserService} userService service providing user functionalities
+   * @param {ActivatedRoute} route route that activated this component
+   * @param {Router} router angular router
    */
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService,  private route: ActivatedRoute, private router: Router) {
+  }
+
+  /**
+   * Inits page
+   */
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.showForm = (params['userId'] === undefined || params['token'] === undefined);
+
+      if (!this.showForm) {
+        this.verifyEmail(params['userId'], params['token']);
+      }
+    });
   }
 
   /**
@@ -29,8 +47,20 @@ export class EmailVerificationComponent {
    *
    * @param {NgForm} verifyForm submitted verification form
    */
-  public async verifyEmail(verifyForm: NgForm): Promise<void> {
+  public async handleVerificationForm(verifyForm: NgForm): Promise<void> {
     if (verifyForm.valid) {
+      //@todo Mario: implement
     }
+  }
+
+  /**
+   * Verifies user's email with provided data
+   *
+   * @param {string} userId user's id
+   * @param {string} token  token to verify email
+   */
+  public async verifyEmail(userId: string, token: string): Promise<void> {
+    await this.userService.verifyEmail(userId, token);
+    await this.router.parseUrl('/login');
   }
 }
