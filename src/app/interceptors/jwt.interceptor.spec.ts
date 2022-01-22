805755194ce1116fbebdef07f239b2a450c97bc5
Adrian Keller <adrian@adrian-keller.de>
2022-01-22 15:41:02 +0100

    finished jwt interceptor test

--- src/app/interceptors/jwt.interceptor.spec.ts
+++ src/app/interceptors/jwt.interceptor.spec.ts
@@ -7,17 +7,26 @@ import { environment } from "../../environments/environment";
 import { JwtInterceptor } from './jwt.interceptor';
 
 describe('JwtInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      JwtInterceptor,
-    ],
-    imports: [
-      HttpClientTestingModule,
-    ],
-  }));
+  let interceptor: JwtInterceptor;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        JwtInterceptor,
+      ],
+      imports: [
+        HttpClientTestingModule,
+      ],
+    });
+
+    interceptor = TestBed.inject(JwtInterceptor);
+  });
+
+  it('should create', () => {
+    expect(interceptor).toBeTruthy();
+  });
 
   it('should add a Bearer token to request', () => {
-    const interceptor: JwtInterceptor = TestBed.inject(JwtInterceptor);
     const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDI1NTE0NjksInVzZXJJZCI6IjI4NDY4OWJmLTFjNzItNGNmYS1iZjA0LTQ3OTUyYzgzOTc3OSIsImlhdCI6MTY0MjU1MDI2OX0.iyLvkH0dvYH9NrB7C2AZUNromKVR1t1ZQqGoKnx0m4g";
     localStorage.setItem(environment.storageKeys.accessToken, accessToken);
 
@@ -30,11 +39,29 @@ describe('JwtInterceptor', () => {
     };
 
     const mockRequest = new HttpRequest('GET', '/test');
+    expect(mockRequest.headers.get('Authorization')).toBe(null);
 
     interceptor.intercept(mockRequest, next).subscribe(() => {
-      expect(mockRequest.method).toBe('GET');
-      expect(mockRequest.url).toBe('/test');
       expect(mockRequest.headers.get('Authorization')).toBe(`Bearer ${accessToken}`);
+    });
+  });
+
+  it('should do nothing when no access token is present', () => {
+    localStorage.clear();
+
+    const next: any = {
+      handle: () => {
+        return new Observable(subscriber => {
+          subscriber.complete();
+        });
+      }
+    };
+
+    const mockRequest = new HttpRequest('GET', '/test');
+    expect(mockRequest.headers.get('Authorization')).toBe(null);
+
+    interceptor.intercept(mockRequest, next).subscribe(() => {
+      expect(mockRequest.headers.get('Authorization')).toBe(null);
     });
   });
 });
