7807651ee8ca02e5c21c96e636e998558996aa2d
Adrian Keller <adrian@adrian-keller.de>
2022-01-25 22:24:16 +0100

    fixed tests

--- src/app/components/user-management/decline/user-decline.component.spec.ts
+++ src/app/components/user-management/decline/user-decline.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterTestingModule } from "@angular/router/testing";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 
 import { UserDeclineComponent } from './user-decline.component';
 
@@ -8,15 +11,20 @@ describe('UserDeclineComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserDeclineComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [
+        UserDeclineComponent,
+      ],
+      imports: [
+        HttpClientModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        NgbActiveModal,
+      ],
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(UserDeclineComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
