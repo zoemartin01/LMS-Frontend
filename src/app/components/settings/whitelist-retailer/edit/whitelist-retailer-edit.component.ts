f93a90354c304e6669cc1a176cfc4ce15e040cec
Mario <mario.lehnerer2001@gmail.com>
2022-01-26 19:09:33 +0100

    implemented basic retailer edit functionality

--- src/app/components/settings/whitelist-retailer/edit/whitelist-retailer-edit.component.ts
+++ src/app/components/settings/whitelist-retailer/edit/whitelist-retailer-edit.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from "@angular/forms";
+import {FormControl, FormGroup} from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
 
 import { AdminService } from "../../../../services/admin.service";
 
 import { WhitelistRetailer } from "../../../../types/whitelist-retailer";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-edit',
@@ -18,42 +19,73 @@ import { WhitelistRetailer } from "../../../../types/whitelist-retailer";
  *
  */
 export class WhitelistRetailerEditComponent implements OnInit {
+  public retailerEditForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+  });
+
+
   public whitelistRetailer: WhitelistRetailer = {
     id: null,
     name: '',
     domains: [],
   }
+  public dirty: boolean = true;
 
   /**
    * Constructor
    * @constructor
    * @param {AdminService} adminService service providing admin functionalities
    * @param {ActivatedRoute} route route that activated this component
+   * @param {NgbActiveModal} activeModal modal containing this component
    */
-  constructor(public adminService: AdminService, private route: ActivatedRoute) {
+  constructor(public adminService: AdminService, private route: ActivatedRoute, public activeModal: NgbActiveModal) {
   }
 
   /**
    * Inits page
    */
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.whitelistRetailer.id = params['id'];
       this.getWhitelistRetailerData();
-    });
   }
 
   /**
    * Gets whitelist retailer data
    */
   public async getWhitelistRetailerData(): Promise<void>{
+    this.adminService.getWhitelistRetailerData(this.whitelistRetailer.id).subscribe({
+      next: res => {
+        this.whitelistRetailer = res;
+        this.retailerEditForm.controls['name'].setValue(res.name);
+      },
+      error: error => {
+        console.error('There was an error!', error);
+      }
+    })
   }
 
   /**
    * Changes data of whitelist retailer
    *
-   * @param {NgForm} whitelistRetailerEditForm submitted edit form
    */
-  public async editWhitelistRetailerData(whitelistRetailerEditForm: NgForm): Promise<void> {
+  public async editWhitelistRetailerData(): Promise<void> {
+    this.adminService.editWhitelistRetailerData(this.whitelistRetailer.id, {
+        name: this.retailerEditForm.controls['name'].value
+      }
+    ).subscribe({
+      next: () => {
+        this.activeModal.close('edited');
+      },
+      error: error => {
+        console.error('There was an error!', error);
+      }
+    });
+  }
+
+  openWhitelistRetailerDomainEditForm() {
+
+  }
+
+  openWhitelistRetailerDomainDeletionDialog() {
+
   }
 }
