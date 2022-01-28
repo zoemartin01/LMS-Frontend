ad0926a86fa5b8a69c23c10aae1fc8d56119a05f
raabs <uoohi@student.kit.edu>
2022-01-28 17:16:22 +0100

    initial create side

--- src/app/components/room-management/create/room-create.component.ts
+++ src/app/components/room-management/create/room-create.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NgForm } from "@angular/forms";
+import {FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
 
 import { RoomService } from "../../../services/room.service";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-room-create',
@@ -15,20 +16,53 @@ import { RoomService } from "../../../services/room.service";
  *
  */
 export class RoomCreateComponent {
+  public roomCreateForm: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required
+    ]),
+    description: new FormControl('', Validators.required),
+    maxConcurrentBookings: new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+      Validators.pattern('[0-9]*')
+    ]),
+    autoAcceptBookings: new FormControl('', Validators.required),
+    /*
+    availableTimeslots: new FormControl(''),
+    unavailableTimeslots: new FormControl(''),
+    TODO tabelle
+     */
+  });
 
   /**
    * Constructor
    * @constructor
    * @param {RoomService} roomService service providing room functionalities
+   * @param {NgbActiveModal} activeModal modal containing this component
    */
-  constructor(public roomService: RoomService) {
+  constructor(public roomService: RoomService,  public activeModal: NgbActiveModal) {
   }
 
   /**
    * Creates room with data
-   *
-   * @param {NgForm} roomCreationForm submitted creation form
    */
-  public async createRoom(roomCreationForm: NgForm): Promise<void> {
+  public async createRoom(): Promise<void> {
+
+    if (this.roomCreateForm.valid) {
+      const name = this.roomCreateForm.value.name;
+      const description = this.roomCreateForm.value.description;
+      const maxConcurrentBookings = this.roomCreateForm.value.maxConcurrentBookings;
+      const autoAcceptBookings = this.roomCreateForm.value.autoAcceptBookings;
+      const availableTimeslots = this.roomCreateForm.value.availableTimeslots;
+      const unavailableTimeslots = this.roomCreateForm.value.unavailableTimeslots;
+
+      this.roomService.createRoom(name, description, maxConcurrentBookings, autoAcceptBookings, availableTimeslots, unavailableTimeslots).subscribe({
+        next: () => {
+          this.activeModal.close('created');
+        }, error: error => {
+          console.error('There was an error!', error);
+        }
+      });
+    }
   }
 }
