b5e90843891fa1c50573ac7d01a6c2dea425f236
Mario <mario.lehnerer2001@gmail.com>
2022-01-28 19:54:09 +0100

    dual use case ultra smart case decision

--- src/app/components/settings/whitelist-retailer/domain-create/whitelist-retailer-domain-create.component.ts
+++ src/app/components/settings/whitelist-retailer/domain-create/whitelist-retailer-domain-create.component.ts
@@ -54,6 +54,11 @@ export class WhitelistRetailerDomainCreateComponent implements OnInit {
 
   public async addDomainToWhitelistRetailer(): Promise<void> {
     if (this.domainCreateForm.valid) {
+      if (this.whitelistRetailer.id === null) {
+        this.activeModal.close(this.domainCreateForm.value.domain);
+        return;
+      }
+
       this.adminService.addDomainToWhitelistRetailer(this.whitelistRetailer.id, this.domainCreateForm.value.domain).subscribe({
         next: (domain: WhitelistRetailerDomain) => {
           if (domain.id !== null) {
