96592d110fa50a48f695effafddd7d268dda6dcf
Mario <mario.lehnerer2001@gmail.com>
2022-02-20 12:19:18 +0100

    filled in pending appointments

--- src/app/components/appointment-management/admin-list/admin-appointment-list.component.spec.ts
+++ src/app/components/appointment-management/admin-list/admin-appointment-list.component.spec.ts
@@ -8,10 +8,11 @@ import {ConfirmationStatus} from "../../../types/enums/confirmation-status";
 import {Observable} from "rxjs";
 import {PagedResponse} from "../../../types/paged-response";
 import {Appointment} from "../../../types/appointment";
+import * as moment from "moment";
 
 
 class MockAppointmentService {
-  public getAllAppointments(limit: number = 0, offset: number = 0, confirmationStatus: ConfirmationStatus|undefined = undefined): Observable<PagedResponse<Appointment>> {
+  public getAllAppointments(limit: number = 0, offset: number = 0, confirmationStatus: ConfirmationStatus | undefined = undefined): Observable<PagedResponse<Appointment>> {
     if (localStorage.getItem('throwError') === 'true') {
       observer.error({
         error: {
@@ -23,23 +24,153 @@ class MockAppointmentService {
     }
 
     const pendingAppointments: Appointment[] = [
-      {
-        /*id: '312d8319-c253-4ee4-8771-a4a8d4a2f411',
-        title: 'Verify Email to confirm account',
-        content: 'You need to click on this link to confirm your account or go to  http://localhost:4200/register/verify-email and enter user-ID: 2fe76781-912d-4cb4-a84c-a0dccf12d74e and token: ywayp.',
-        correspondingUrl: ' http://localhost:4200/register/verify-email/2fe76781-912d-4cb4-a84c-a0dccf12d74e/ywayp',
-        correspondingUrlText: 'Verify Email',
-        readStatus: false,*/
-      },
-      {
-        /*id: 'b16a9b6a-aa07-41a6-ab5a-c972c2894458',
-        title: 'Verify Email to confirm account',
-        content: 'You need to click on this link to confirm your account or go to  http://localhost:4200/register/verify-email and enter user-ID: 2fe76781-912d-4cb4-a84c-a0dccf12d74e and token: ywayp.',
-        correspondingUrl: ' http://localhost:4200/register/verify-email/2fe76781-912d-4cb4-a84c-a0dccf12d74e/ywayp',
-        correspondingUrlText: 'Verify Email',
-        readStatus: false,*/
-      },
-    ];
+        {
+          id: "c3a70a44-374c-46a9-be05-a3f6ef4e39a5",
+          start: moment("2022-02-14T13:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          end: moment("2022-02-14T16:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          type: 1,
+          seriesId: "eef5fadc-53d9-4a49-83be-e55b2f94bb8e",
+          amount: 4,
+          timeSlotRecurrence: 3,
+          confirmationStatus: 1,
+          maxStart: moment("2022-03-07T13:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          room: {
+            id: "c7231328-203e-43f5-9ac1-d374d90484ac",
+            name: "Test room",
+            description: "room to test",
+            maxConcurrentBookings: 1,
+            autoAcceptBookings: true
+          },
+          user: {
+            id: "ecaf341e-e600-4e4e-adab-a7e016c993ac",
+            email: "admin@test.com",
+            firstName: "Admin",
+            lastName: "Admin",
+            role: 3,
+            emailVerification: true,
+            isActiveDirectory: false,
+            notificationChannel: 3
+          }
+        },
+        {
+          id: "3f7af855-ad57-4a4c-81e7-769ba90f9e76",
+          start: moment("2022-02-16T05:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          end: moment("2022-02-16T06:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          type: 1,
+          seriesId: null,
+          amount: 1,
+          timeSlotRecurrence: 1,
+          confirmationStatus: 1,
+          maxStart: undefined,
+          room: {
+            id: "c7231328-203e-43f5-9ac1-d374d90484ac",
+            name: "Test room",
+            description: "room to test",
+            maxConcurrentBookings: 1,
+            autoAcceptBookings: true
+          },
+          user: {
+            id: "ecaf341e-e600-4e4e-adab-a7e016c993ac",
+            email: "admin@test.com",
+            firstName: "Admin",
+            lastName: "Admin",
+            role: 3,
+            emailVerification: true,
+            isActiveDirectory: false,
+            notificationChannel: 3
+          }
+        },
+        {
+          id: "e401b04a-3688-49d8-b36a-f38e1942ff3f",
+          start: moment("2022-02-21T13:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          end: moment("2022-02-21T16:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          type: 1,
+          seriesId: "eef5fadc-53d9-4a49-83be-e55b2f94bb8e",
+          amount: 4,
+          timeSlotRecurrence: 3,
+          confirmationStatus: 1,
+          maxStart: moment("2022-03-07T13:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          room: {
+            id: "c7231328-203e-43f5-9ac1-d374d90484ac",
+            name: "Test room",
+            description: "room to test",
+            maxConcurrentBookings: 1,
+            autoAcceptBookings: true
+          },
+          user: {
+            id: "ecaf341e-e600-4e4e-adab-a7e016c993ac",
+            email: "admin@test.com",
+            firstName: "Admin",
+            lastName: "Admin",
+            role: 3,
+            emailVerification: true,
+            isActiveDirectory: false,
+            notificationChannel: 3
+          }
+        },
+        {
+          id: "1415b732-328a-46a4-abcb-c1493252a9cc",
+          start: moment("2022-02-28T13:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          end: moment("2022-02-28T16:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          type: 1,
+          seriesId: "eef5fadc-53d9-4a49-83be-e55b2f94bb8e",
+          amount: 4,
+          timeSlotRecurrence: 3,
+          confirmationStatus: 1,
+          maxStart: moment("2022-03-07T13:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          room: {
+            id: "c7231328-203e-43f5-9ac1-d374d90484ac",
+            name: "Test room",
+            description: "room to test",
+            maxConcurrentBookings: 1,
+            autoAcceptBookings: true
+          },
+          user: {
+            id: "ecaf341e-e600-4e4e-adab-a7e016c993ac",
+            email: "admin@test.com",
+            firstName: "Admin",
+            lastName: "Admin",
+            role: 3,
+            emailVerification: true,
+            isActiveDirectory: false,
+            notificationChannel: 3
+          }
+        },
+        {
+          id: "a229732c-32a3-4aee-b678-3b52f6b8a4a0",
+          start: moment("2022-03-07T13:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          end: moment("2022-03-07T16:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          type: 1,
+          seriesId: "eef5fadc-53d9-4a49-83be-e55b2f94bb8e",
+          amount: 4,
+          timeSlotRecurrence: 3,
+          confirmationStatus: 1,
+          maxStart: moment("2022-03-07T13:00:00.000Z", 'YYYY-MM-DDTHH:mm'),
+          room: {
+            id: "c7231328-203e-43f5-9ac1-d374d90484ac",
+            name: "Test room",
+            description: "room to test",
+            maxConcurrentBookings: 1,
+            autoAcceptBookings: true
+          },
+          user: {
+            id: "ecaf341e-e600-4e4e-adab-a7e016c993ac",
+            email: "admin@test.com",
+            firstName: "Admin",
+            lastName: "Admin",
+            role: 3,
+            emailVerification: true,
+            isActiveDirectory: false,
+            notificationChannel: 3
+          }
+        }
+      ]
+    ;
+
+    const acceptedAppointments: Appointment[];
+    const deniedAppointments: Appointment[];
+
+
     if (confirmationStatus == ConfirmationStatus.pending) {
       observer.next(pendingAppointments);
     }
@@ -49,50 +180,7 @@ class MockAppointmentService {
     if (confirmationStatus == ConfirmationStatus.denied) {
       observer.next(deniedAppointments);
     }
-  });
-
-
   }
-
-  getMessages(): Observable<Message[]> {
-    return new Observable((observer) => {
-      if (localStorage.getItem('throwError') === 'true') {
-        observer.error({
-          error: {
-            error: {
-              message: 'Unknown Error.',
-            }
-          }
-        });
-      }
-
-      const messages: Message[] = [
-        {
-          id: '312d8319-c253-4ee4-8771-a4a8d4a2f411',
-          title: 'Verify Email to confirm account',
-          content: 'You need to click on this link to confirm your account or go to  http://localhost:4200/register/verify-email and enter user-ID: 2fe76781-912d-4cb4-a84c-a0dccf12d74e and token: ywayp.',
-          correspondingUrl: ' http://localhost:4200/register/verify-email/2fe76781-912d-4cb4-a84c-a0dccf12d74e/ywayp',
-          correspondingUrlText: 'Verify Email',
-          readStatus: false,
-        },
-        {
-          id: 'b16a9b6a-aa07-41a6-ab5a-c972c2894458',
-          title: 'Verify Email to confirm account',
-          content: 'You need to click on this link to confirm your account or go to  http://localhost:4200/register/verify-email and enter user-ID: 2fe76781-912d-4cb4-a84c-a0dccf12d74e and token: ywayp.',
-          correspondingUrl: ' http://localhost:4200/register/verify-email/2fe76781-912d-4cb4-a84c-a0dccf12d74e/ywayp',
-          correspondingUrlText: 'Verify Email',
-          readStatus: false,
-        },
-      ];
-
-      observer.next(messages);
-    });
-  }
-getAllAppointments(pending)
-  getAllAppointments(accepted)
-  getAllAppointments(denied)
-
-
 }
 
 class MockUserService {
@@ -114,7 +202,7 @@ describe('AdminAppointmentListComponent', () => {
         RouterTestingModule,
       ],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
