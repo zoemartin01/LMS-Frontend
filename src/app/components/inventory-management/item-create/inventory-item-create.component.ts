5ed460886fb82177b71172293abc5bff141c0348
Mario <mario.lehnerer2001@gmail.com>
2022-03-05 14:42:01 +0100

    inventory and order error stuff

--- src/app/components/inventory-management/item-create/inventory-item-create.component.ts
+++ src/app/components/inventory-management/item-create/inventory-item-create.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 
-import { InventoryService } from "../../../services/inventory.service";
+import {InventoryService} from "../../../services/inventory.service";
 
-import { InventoryItem } from "../../../types/inventory-item";
+import {InventoryItem} from "../../../types/inventory-item";
+import {UtilityService} from "../../../services/utility.service";
 
 @Component({
   selector: 'app-inventory-item-create',
@@ -23,39 +24,42 @@ export class InventoryItemCreateComponent {
     description: new FormControl(''),
     quantity: new FormControl('', [
       Validators.required,
-      Validators.min(0),
     ])
   });
+  public errorMessage = '';
 
   /**
    * Constructor
    * @constructor
    * @param {InventoryService} inventoryService service providing inventory functionalities
+   * @param {UtilityService} utilityService service providing utility functionalities
    * @param {NgbActiveModal} activeModal modal containing this component
    */
-  constructor(public inventoryService: InventoryService, public activeModal: NgbActiveModal) {
+  constructor(public inventoryService: InventoryService, public utilityService: UtilityService, public activeModal: NgbActiveModal) {
   }
+
 
   /**
    * Opens inventory creation form
    */
   public async createInventoryItem(): Promise<void> {
-    if (this.createInventoryItemForm.valid) {
-      const name = this.createInventoryItemForm.value.itemName;
-      const description = this.createInventoryItemForm.value.description;
-      const quantity = +this.createInventoryItemForm.value.quantity;
-      this.inventoryService.createInventoryItem(name, description,quantity).subscribe({
-        next: (inventoryItem: InventoryItem) => {
-          if (inventoryItem.id !== null ) {
-            this.activeModal.close(`created ${inventoryItem.id}`);
-          }
-        },
-        error: error => {
-          console.error('There was an error!', error);
-        }
-      })
-    } else {
-      console.error('Invalid form values');
+    this.errorMessage = '';
+    if (this.createInventoryItemForm.invalid) {
+      this.errorMessage = 'You need to fill in all required fields!'
+      return;
     }
+    const name = this.createInventoryItemForm.value.itemName;
+    const description = this.createInventoryItemForm.value.description;
+    const quantity = +this.createInventoryItemForm.value.quantity;
+    this.inventoryService.createInventoryItem(name, description, quantity).subscribe({
+      next: (inventoryItem: InventoryItem) => {
+        if (inventoryItem.id !== null) {
+          this.activeModal.close(`created ${inventoryItem.id}`);
+        }
+      },
+      error: error => {
+        this.errorMessage = this.utilityService.formatErrorMessage(error);
+      }
+    })
   }
 }
--- src/app/components/order-management/accept/order-accept.component.ts
+++ src/app/components/order-management/accept/order-accept.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
-import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 
-import { OrderService } from "../../../services/order.service";
+import {OrderService} from "../../../services/order.service";
 
-import { Order } from "../../../types/order";
-import { UserRole } from "../../../types/enums/user-role";
-import { NotificationChannel } from "../../../types/enums/notification-channel";
-import { OrderStatus } from "../../../types/enums/order-status";
+import {Order} from "../../../types/order";
+import {UserRole} from "../../../types/enums/user-role";
+import {NotificationChannel} from "../../../types/enums/notification-channel";
+import {OrderStatus} from "../../../types/enums/order-status";
+import {UtilityService} from "../../../services/utility.service";
 
 @Component({
   selector: 'app-order-accept',
@@ -42,14 +43,16 @@ export class OrderAcceptComponent implements OnInit {
     },
     status: OrderStatus.unknown,
   };
+  public errorMessage = '';
 
   /**
    * Constructor
    * @constructor
    * @param {OrderService} orderService service providing order functionalities
+   * @param {UtilityService} utilityService service providing utility functionalities
    * @param {NgbActiveModal} activeModal modal containing this component
    */
-  constructor(public orderService: OrderService, public activeModal: NgbActiveModal) {
+  constructor(public orderService: OrderService, public utilityService: UtilityService, public activeModal: NgbActiveModal) {
     this.orderAcceptForm.disable();
   }
 
@@ -63,7 +66,7 @@ export class OrderAcceptComponent implements OnInit {
   /**
    * Gets all data of order
    */
-  public async getOrderData() : Promise<void> {
+  public async getOrderData(): Promise<void> {
     this.orderService.getOrderData(this.order.id).subscribe({
       next: res => {
         this.order = res;
@@ -86,13 +89,14 @@ export class OrderAcceptComponent implements OnInit {
   /**
    * Accept order
    */
-  public async acceptOrder() : Promise<void> {
+  public async acceptOrder(): Promise<void> {
+    this.errorMessage = '',
     this.orderService.acceptOrderRequest(this.order.id).subscribe({
       next: () => {
         this.activeModal.close('accepted');
       },
       error: error => {
-        console.error('There was an error!', error);
+        this.errorMessage = this.utilityService.formatErrorMessage(error);
       },
     });
   }
